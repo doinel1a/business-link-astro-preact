91dfa249fd0e0dfe939d125efbc2a34046116bc2
doinel1a <doinel1atanasiu@gmail.com>
2022-10-09 19:12:38 +0200

    Sitemap package added and implemented

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
+import meta_tags from './src/config/meta_tags';
 
 export default defineConfig({
-	integrations: [preact(), tailwind()],
+	site: meta_tags.app_url,
+	integrations: [preact(), tailwind(), sitemap()],
 });
